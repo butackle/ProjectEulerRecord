bb03f6c8233a9bd19186b487d2a25a87f6f67fdf
puru <butackle@gmail.com>
2024-04-13 16:08:44 +0900

    add comment

--- 23/calcImpossibleSum.ts
+++ 23/calcImpossibleSum.ts
@@ -1,3 +1,9 @@
+/**
+ * 2つの過剰数の和で書き表せない正の整数の総和を求める
+ *
+ * @param abundantNumbers
+ * @param limit
+ */
 export const calcImpossibleSum = (abundantNumbers: number[], limit: number): number => {
   let sum = 0;
 
@@ -17,6 +23,13 @@ export const calcImpossibleSum = (abundantNumbers: number[], limit: number): num
   return sum;
 }
 
+/**
+ * 対象の数字が2つの過剰数の和で書き表せないかを調べる。
+ * 書き表せる場合には0を返して、書き表せない場合には対象の数字を返す。
+ *
+ * @param targetNumber
+ * @param abundantNumbers
+ */
 const competeAbundantNumbers = (targetNumber: number, abundantNumbers: number[]): number => {
   for (const opponent1 of abundantNumbers) {
     if (opponent1 > targetNumber) {
